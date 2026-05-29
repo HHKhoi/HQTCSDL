ed3f324e14162bb3e2adae7c3d61136c567c48ad
HHKhoi <hahukhoi2k5@gmail.com>
2026-05-29 21:45:02 +0700

    chore: add console.error to server errorHandler

--- server/shared/utils/errorHandler.js
+++ server/shared/utils/errorHandler.js
@@ -2,6 +2,8 @@ exports.errorHandler = (err, req, res, _next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
+  console.error('API Error:', err);
+
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
